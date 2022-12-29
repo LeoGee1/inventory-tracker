3782d855929800fb1b1f928fb8532a75bc45dd9a
LeoGee1 <ucheleonard2000@gmail.com>
2022-12-29 16:08:24 +0100

    added more sample items images

--- tracker.js
+++ tracker.js
@@ -53,16 +53,53 @@ tabs.addEventListener ('click', (e) => {
 //this is to make the tab bottom border be visible on click
 tabbed.forEach(tab => {
     tab.addEventListener('click', () =>{
-        document.querySelector('.one').classList.remove('one')
-        tab.classList.add('one')
-    })
+        document.querySelector('.one').classList.remove('one');
+        tab.classList.add('one');
+    });
+});
+
+// this is to display product preview image for upload
+const preview = document.querySelector('#file');
+const displayImage = document.querySelector('#labelFile');
+var uploaded_image = '';
+
+
+preview.addEventListener('change', function() {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => {
+        uploaded_image = reader.result;
+        displayImage.style.backgroundImage = `url(${uploaded_image})`
+    });
+    reader.readAsDataURL(this.files[0]);
+});
+
+// this is to add product image, product name to the home page
+const addBtn = document.querySelector('#addBtn');
+
+
+addBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const NameValue = document.querySelector('#productName').Value.trim()
+    const descriptionValue = document.querySelector('#productDescription').value.trim()
+    if (NameValue.length === 0 || nameValue.length > 15) {
+        return;
+    } else if (DescriptionValue.length === 0 || descriptionValue.length > 200) {
+        return
+    }else {
+        // create elements
+        const items = document.createElement('div');
+        const upper = document.createElement('div');
+        const noItem = document.createElement('i');
+        const itemsMenu = document.createElement('div');
+        const itemName = document.createElement('h3');
+        const info = document.createElement('i');
+        const cancel = document.createElement('i');
+
+        //add text contents
+
+        // create classes
+    }
 })
-
-
-
-
-
-
 
 
 
